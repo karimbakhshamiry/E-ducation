b96be14da14a494c9200e6499055554ec6459fae
karimbakhshamiry <karimbakhshamiry.123@gmail.com>
2022-05-21 02:00:49 +0430

    faqs has been completed
    Frequently asked questions sections has been completed with its design.
    
    js code has been written to expand the answer, but I couldn't add much of smooth animation to show the answer.
    
    besides, changing a css property (box-sizing) to border-box brought an issue with the layout. So it needs to be fixed in next commit.

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,3 +3,48 @@ const navBar = document.querySelector('.nav')
 window.addEventListener('scroll', () => {
     navBar.classList.toggle('window-scroll', scrollY > 100)
 })
+
+document.querySelectorAll('.expand-question').forEach(element => {
+    element.addEventListener('click', () => {
+
+        // hide all other faqs answers
+        const currentFaq = document.querySelector('.show-answer')
+
+        if (currentFaq) {
+            const elementAnswer = element.parentElement.children[1].children[1]
+
+            if (currentFaq.isEqualNode(elementAnswer)) {
+                
+                toggleShowFaqAnswer(element)
+
+            } else {
+
+                currentFaq.classList.toggle('show-answer')
+    
+                currentFaq.parentElement.parentElement.children[0].classList.remove('fa-minus')
+                currentFaq.parentElement.parentElement.children[0].classList.add('fa-plus')
+                
+                // show the clicked question's answer
+                toggleShowFaqAnswer(element)
+
+            }
+        } else {
+            toggleShowFaqAnswer(element)
+        }
+
+    })
+})
+
+function toggleShowFaqAnswer(crossmark) {
+    // show the clicked question's answer
+    crossmark.parentElement.children[1].children[1].classList.toggle('show-answer')
+        
+    // handle the plus/minus sign
+    if (crossmark.classList.contains('fa-plus')) {
+        crossmark.classList.remove('fa-plus')
+        crossmark.classList.add('fa-minus')
+    } else {
+        crossmark.classList.remove('fa-minus')
+        crossmark.classList.add('fa-plus')
+    }
+}
